263f48a8c6a5a058ed7505240bf03de87b497e71
Nigel Purves <nigel.purves@googlemail.com>
2014-09-21 15:06:30 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sweepstakes::Application.config.secret_key_base = 'cf6988601ad77037fc71f128e91c58f6849690ec18ba2892fcb62184655e1a2d179f71a2dc722c7dc8a6b5d2bc6eafc60959b5e8250e729b0b8683d82fe2abb9'
